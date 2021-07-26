f321a9bb2ece48eed3270f09d2f32275371874a4
Simon Kuforiji <simonkuforiji@gmail.com>
2021-07-26 02:39:09 +0100

    add google OAuth

--- app.js
+++ app.js
@@ -3,10 +3,15 @@ const express = require("express");
 const ejs = require("ejs");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
-//const encrypt = require("mongoose-encryption");
-//const md5 = require("md5");
-const bcrypt = require("bcrypt");
-const saltRounds = 10;
+const session = require("express-session");
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose");
+const findOrCreate = require("mongoose-findorcreate");
+//google
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+//facebook
+const FacebookStrategy = require('passport-facebook').Strategy;
+
 
 const app = express();
 
@@ -15,74 +20,174 @@ app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
+app.use(session({
+    secret: process.env.SECRET,
+    resave: false,
+    saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
 mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false});
+
+mongoose.set('useCreateIndex', true);
 
 const userSchema = new mongoose.Schema({
     email: String,
-    password: String
+    password: String,
+    wisp: String,
+    //google
+    googleId: String,
+    //facebook
+    facebookId: String
 });
 
-//userSchema.plugin(encrypt, {secret: process.env.SECRETTEXT, encryptedFields: ["password"]});
+userSchema.plugin(passportLocalMongoose);
+userSchema.plugin(findOrCreate);
 
 const User = new mongoose.model("User", userSchema);
+
+passport.use(User.createStrategy());
+
+passport.serializeUser(function(user, done) {
+    done(null, user.id);
+});
+  
+passport.deserializeUser(function(id, done) {
+    User.findById(id, function(err, user) {
+        done(err, user);
+    });
+});
+
+//google
+passport.use(new GoogleStrategy({
+    clientID: process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    callbackURL: "http://localhost:3000/auth/google/wisp"
+  },
+  function(accessToken, refreshToken, profile, cb) {
+      //console.log(profile);
+    User.findOrCreate({ googleId: profile.id }, function (err, user) {
+        return cb(err, user);
+      });
+    }
+));
+//facebook
+passport.use(new FacebookStrategy({
+    clientID: process.env.FACEBOOK_APP_ID,
+    clientSecret: process.env.FACEBOOK_APP_SECRET,
+    callbackURL: "http://localhost:3000/auth/facebook/wisp"
+  },
+  function(accessToken, refreshToken, profile, cb) {
+    User.findOrCreate({ facebookId: profile.id }, function (err, user) {
+      return cb(err, user);
+    });
+  }
+));
 
 app.get("/", function(req, res){
     res.render("home");
 });
 
+//google
+app.get("/auth/google",
+  passport.authenticate("google", { scope: ["profile"] })
+);
+
+//google
+app.get("/auth/google/wisp", 
+  passport.authenticate("google", { failureRedirect: "/login" }),
+  function(req, res) {
+    // Successful authentication, redirect to wisps.
+    res.redirect("/wisps");
+    }
+);
+
 app.get("/register", function(req, res){
     res.render("register");
+});
+
+app.get("/wisps", function(req, res){
+    if(req.isAuthenticated()){
+        User.find({wisp: {$ne: null}}, function(err, foundArray){
+            if(err){
+                console.log(err);
+            } else {
+                res.render("wisps", {wispsArray: foundArray});
+            }
+        })
+    } else {
+        res.redirect("/login");
+    }
 });
 
 app.get("/login", function(req, res){
     res.render("login");
 });
 
-app.post("/register", function(req, res){
-    const username = req.body.username;
-    const userPassword = req.body.password;
+app.get("/logout", function(req, res){
+    req.logout();
+    res.redirect("/");
+});
 
-    User.findOne({email: username}, function(err, foundObject){
-        if(!err){
-            if(!foundObject){
-                bcrypt.hash(userPassword, saltRounds, function(err, hash) {
-                    const newUser = new User({
-                        email: username,
-                        password: hash
-                    });
-                    newUser.save();
-                    res.render("secrets");
-                });
-            } else{
-                console.log("user already exists");
-                res.redirect("/");
-            }
-        } else{
+app.get("/submit", function(req, res){
+    if(req.isAuthenticated()){
+        res.render("submit");
+    } else {
+        res.redirect("/login");
+    }
+});
+
+app.post("/register", function(req, res){
+    User.register({username: req.body.username}, req.body.password, function(err, newUser){
+        if(err){
             console.log(err);
+            res.redirect("/register");
+        } else {
+            //console.log("user created");
+            console.log(newUser);
+            passport.authenticate("local")(req, res, function(){
+                //console.log("authentication successful");
+                res.redirect("/wisps");
+            });
         }
-    });
+    })
 });
 
 app.post("/login", function(req, res){
-    const username = req.body.username;
-    const userPassword = req.body.password;
-
-    User.findOne({email: username}, function(err, foundObject){
-        if(!err){
-            bcrypt.compare(userPassword, foundObject.password, function(err, result) {
-                if(result){
-                    res.render("secrets");
-                } else{
-                    console.log("wrong password");
-                    res.redirect("/login");
-                }
-            });
-        } else{
+    const user = new User({
+        email: req.body.username,
+        password: req.body.password
+    });
+    req.login(user, function(err){
+        if(err){
             console.log(err);
+        } else {
+            //console.log("user exists");
+            passport.authenticate("local")(req, res, function(){
+                //console.log("authentication successful");
+                res.redirect("/wisps");
+            });
         }
     });
 });
 
+app.post("/submit", function(req, res){
+    const submittedWisp = req.body.wisp;
+    //find the user submitting
+    User.findOne({_id: req.user.id}, function(err, foundObject){
+        if(err){
+            console.log(err);
+        } else {
+            foundObject.wisp = submittedWisp;
+            foundObject.save(function(){
+                res.redirect("/wisps");
+                //console.log(foundObject.wisp);
+            });
+        }
+    });
+});
 
 let port = 3000;
 app.listen(port, function(){
